a79a06d3616f4ca5fc9dd97052b85dc2b99ded63
Alexandre Retamero <alexandreretamero@gmail.com>
2023-06-21 19:43:08 -0300

    teste

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,30 +1,30 @@
-// pages/cart.tsx
+// // pages/cart.tsx
 
-import CartTotal from '@/components/CardTotal';
-import CartTable from '@/components/CartTable';
-import { NextPage } from 'next';
-import Head from 'next/head';
-import { Container } from 'reactstrap';
+// import CartTotal from '@/components/CardTotal';
+// import CartTable from '@/components/CartTable';
+// import { NextPage } from 'next';
+// import Head from 'next/head';
+// import { Container } from 'reactstrap';
 
-const Cart: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Carrinho</title>
-        <meta name="description" content="Meu carrinho de compras" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// const Cart: NextPage = () => {
+//   return (
+//     <>
+//       <Head>
+//         <title>Carrinho</title>
+//         <meta name="description" content="Meu carrinho de compras" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-      <main>
-        <Container className="mb-5">
-          <h1 className="my-5">Carrinho</h1>
+//       <main>
+//         <Container className="mb-5">
+//           <h1 className="my-5">Carrinho</h1>
 
-          <CartTable />
-          <CartTotal />
-        </Container>
-      </main>
-    </>
-  );
-};
+//           <CartTable />
+//           <CartTotal />
+//         </Container>
+//       </main>
+//     </>
+//   );
+// };
 
-export default Cart;
+// export default Cart;
